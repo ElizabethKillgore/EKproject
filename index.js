a4d8d585347229cc992d0b0a12b15aac92b8fdaf
Elizabeth <ek@elizabethkillgore.com>
2023-11-14 15:51:00 -0500

    changed around the CSS file and the html file to be cleaner and updated js file with comments

--- index.js
+++ index.js
@@ -33,8 +33,9 @@ function renderOnePainting(paintingData) {
     // console.log(paintingDate)
 }
  
+//I got the images from The Art Institute of Chicago as a border for art decoration for the website
+//I have put a comment form for users to comment on a specific piece of art.  I want to make sure that the comment form can be applied to the painting or piece of art the user wants to comment on by keeping their comments.  They can keep a list of their comments or erase them individually.  I would have done a POST, but trying to keep it simple. 
 
-//I would like to put a comment form for each paintingCard.  Or, another way is to make sure that the comment form can be applied to the painting the user wants to comment on by keeping their comments.  If I do that I may need to display the painting ID number.  I don't want to get too complicated, need to keep this SPA simple so I can complete it quickly.  Time is of the essence, in this case.  I get the material and supremely enjoy the process herein, but I am way behind in the course schedule due to circumstances that were beyond my control.
 const inputForm = document.querySelector("form")
 
 inputForm.addEventListener("submit", (e) => {
@@ -58,7 +59,7 @@ function addUserComments(comments) {
 //     .then(res => res.json())
 //     .then(data => console.log(data))
 
-// In working out this fetch data to put it to good use easily, I will limit the GET request to reflect the info I want to use on my API rather than all the data for each painting. That worked out well, much less data.
+// In working out this fetch data to put it to good use easily, I will limit the GET request to reflect the info I want to use on my API rather than all the data for each painting. I want just id, title, artist info, date first.  That worked out well, much less data.
 
 //This fetch limits the info to only the fields that I want right now.
 // fetch('https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display,main_reference_number')
@@ -83,28 +84,12 @@ function addUserComments(comments) {
 //     .then(artData => (console.log(artData.data)))
     
 // }) 
-// Two issues that I ran into with the API 'Art Institue of Chicago'.  One, is that the twelve they send in the fetch are random and change as you refresh your page(browser).  Two, is that the image info must first be in a GET request asking for the image ID number associated with the element ID number for the painting.  Then you must use that very long image ID number to get your image.  Thus, I hardcoded twelve paintings into the HTML and used those id numbers as src to get each of the twelve images.  Not practical as I can't associate the images with the painting info through a fetch. 
+// Two issues that I ran into with the API 'Art Institue of Chicago'.  One, is that the twelve they send in the fetch are random and change as you refresh your page(browser).  Two, is that the image info must first be in a GET request asking for the image ID number associated with the element ID number for the painting.  Then you must use that very long image ID number to get your image.  Thus, I hardcoded twelve paintings into the HTML and used those id numbers as src to get each of the twelve images.  Not practical as I can't associate the images with the painting info through a fetch.  So instead I am using the images to outline the website as borders.  I don't have images with the twelve chosen works of art.  But I will take advantage of the fetch returning different works of art when I refresh my work page and the fetch re-fetches different works of art.  I will use that as a way to make the site more interesting with changing pieces popping up periodically.  Eventually, I can redo the app where the fetch will grab the images along with the works of art info.  But for now, must keep it simple.
 
 
 //Alright, success, that worked.  I got only the array data I wanted.  Next I have to get this data displayed.
 
 //I added the images to my HTML so they are displayed on the page.  I did this by fetching the image id info in the manner outlined by the API.  The images come separately than the other info previously fetched.
 
-//I had to add the addEventListener DOMContentLoaded at the top, so that the images would appear and the info would load in the proper order.
+//I had to add the addEventListener DOMContentLoaded at the top, so that the images would appear and the info would load asynchronously.
 
-// const EMPTY_HEART = '♡'
-// const FULL_HEART = '♥'
-
-// document.addEventListener("click", (e) => {
-//     const heart = e.target
-//     fetch() 
-//     .then(() => { 
-//           if (heart.innerText === EMPTY_HEART) {
-//          heart.innerText = FULL_HEART
-//          heart.className = "activated-heart"
-//           } else {
-//          heart.innerText = EMPTY_HEART
-//          heart.className = ""
-//           }
-//         })
-// })       
