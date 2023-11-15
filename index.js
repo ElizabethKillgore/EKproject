c0eb94b1369b729b445b4c3589ab1c286e7f9989
Elizabeth <ek@elizabethkillgore.com>
2023-11-15 15:01:33 -0500

    Added to my comments in my index.js file and squared up code for final submission

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ inputForm.addEventListener("submit", (e) => {
     e.preventDefault()
      addUserComments(e.target["inputComment"].value)
 }) 
+
 function addUserComments(comments) {
     let li = document.createElement('li')
     let btn = document.createElement('button')
@@ -50,46 +51,47 @@ function addUserComments(comments) {
 }
 })
 
+//I am mindful not to use repetitive code, as per the instructions for this final project, but I include all of the other fetches, hereinbelow, to show the process and all of the info that I received for future reference. 
 
 //I have used the images from The Art Institute of Chicago as a border for art decoration for the website
-//I have put a comment form for users to comment on a specific piece of art of the twelve fetched.  I want to make sure that the comment form can be applied to the painting or piece of art the user wants to comment on by keeping their comments.  They can keep a list of their comments or erase them individually.  I would have done a POST, but trying to keep it simple. 
+//I have put a comment form for users, with an event listener, to comment on a specific piece of art of the twelve fetched.  I want to make sure that the comment form can be applied to the paintings or pieces of art the user wants to comment and then let the user keep their comments, as they add more.  They can keep a list of their comments, or erase them individually, with another event listener.  I would have done a POST, so when the page refreshes the comments stay until erased by the user, but trying to keep it simple. 
 
-// This fetch is the first getting the whole load of data for group of twelve.
-// fetch('https://api.artic.edu/api/v1/artworks') 
-//     .then(res => res.json())
-//     .then(data => console.log(data))
+//This fetch is the first getting the whole load of data for group of twelve.
+fetch('https://api.artic.edu/api/v1/artworks') 
+    .then(res => res.json())
+    .then(data => console.log(data))
 
-// In working out this fetch data to put it to good use easily, I will limit the GET request to reflect the info I want to use on my API rather than all the data for each painting. I want just id, title, artist info, date first.  That worked out well, much less data.
+// In working out this fetch data to put it to good use easily, I will limit the GET request to reflect the info I want to use on my API rather than all the data for each painting. I want just id, title, artist info, date, first.  That worked out well, much less data.
 
 //This fetch limits the info to only the fields that I want right now.
-// fetch('https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display,main_reference_number')
-//     .then(res => res.json())
-//     .then(artData => console.log(artData))
+fetch('https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display,main_reference_number')
+    .then(res => res.json())
+    .then(artData => console.log(artData))
 
 //Now, I need to figure out how to console.log the array only.  Proving trickier than I thought.
 
 //But first, here, I will fetch the data with the image ID so that I can set up the html with the images.
 
 //This fetch is the image_id for the first twelve that I originally fetched at the beginning of my project work.
-// fetch('https://api.artic.edu/api/v1/artworks?fields=id,title,image_id')
-//     .then(res => res.json())
-//     .then(data => console.log(data))
+fetch('https://api.artic.edu/api/v1/artworks?fields=id,title,image_id')
+    .then(res => res.json())
+    .then(data => console.log(data))
 //Ok got that working.  Now get only the array info without all the other data. 
 
-//This fetch is the array only, not the other objects.
+//This fetch is the array of twelve, only, not the other data.
+   
+fetch('https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display,main_reference_number')
+    .then(res => res.json())    
+    .then(artData => (console.log(artData.data)))
+    
 
-    
-// fetch('https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display,main_reference_number')
-//     .then(res => res.json())    
-//     .then(artData => (console.log(artData.data)))
-    
-// }) 
-// Two issues that I ran into with the API 'Art Institue of Chicago' as my Fetch source.  ARTIC has a built-in limit of twevle works at a time.    
-//One, is that the twelve they send in the fetch are random and change as you refresh your page(browser).  (You can get more or less, but twelve works for my purposes.)
-// Two, is they do not include images with the art pieces information and that the image info must first be in a GET request asking for the image ID number associated with the element ID number for the painting.  Then you must use that very long image ID number to get your image.  Thus, I hardcoded twelve paintings into the HTML and used those id numbers as src to get each of the twelve images.  Not practical as I can't associate the images with the painting info through a simple fetch.  So instead I am using the images to outline the website as borders.  I don't have images with the twelve chosen works of art.  But I will take advantage of the fetch returning different works of art when I refresh my work page and the fetch re-fetches different works of art.  I will use that as a way to make the site more interesting with changing pieces popping up periodically.  Eventually, I can redo the app where the fetch will grab the images along with the works of art info.  But for now, must keep it simple.
+// Two issues that I ran into with the API 'Art Institue of Chicago' as my Fetch source.  ARTIC has a built-in limit of twelve works at a time.    
+//One issue is that the twelve they send in the fetch are random and change periodically as you refresh your page(browser).  (You can get more or less, but twelve works for my purposes.)
+// Two, is that they do not include images with the art pieces information, the images come separately than the other info previously fetched. The image info must first be in a GET request asking for the image ID number associated with the element ID number for the painting.  Then you must use that very long image ID number to get your image.  I fetched the image id info in the manner outlined by the API.  Then, I hardcoded twelve paintings into the HTML and used those id numbers as src to get each of the twelve images.  Not practical as I can't associate the images with the painting info through a simple fetch.  So instead I am using the images to outline the website as border decoration.  I don't have images with the twelve chosen works of art.  But I will take advantage of the fetch returning different works of art when I refresh my work page and the fetch re-fetches different works of art.  I will use that as a way to make the site more interesting with changing pieces popping up periodically.  Eventually, I can redo the app where the fetch will grab the images along with the works of art info.  But for now, must keep it simple.
 
 
 //Alright, success, that worked.  I got only the array data I wanted.  Next I have to get this data displayed.
+//Ok, that worked.
 
 //I added the images to my HTML so they are displayed on the page.  I did this by fetching the image id info in the manner outlined by the API.  The images come separately than the other info previously fetched.
 
